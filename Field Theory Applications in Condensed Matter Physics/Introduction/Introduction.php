ba5b6be46ed7cba049e4496f3f4923d8d2b22f41
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-08-16 20:40:22 +0530

    Intro

---
+++ Field Theory Applications in Condensed Matter Physics/Introduction/Introduction.php
@@ -0,0 +1,86 @@
+
+Field Theory Applications in Condensed Matter Physics
+
+
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><br>Hey there, I'm Riddhiman Bhattacharya FRSA. Hope you're all healthy,  hale and hearty.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>I've got this idea buzzing in my head lately. I want to write a series of blogs on <span style="text-decoration: underline"><em><strong>Applications of Field Theory in Condensed Matter Physic</strong>s</em></span>. I know it sounds a bit technical, and is a very vast area of <a href="https://www.routledge.com/Field-Theories-in-Condensed-Matter-Physics/Rao/p/book/9780750308762#:~:text=Description,Hall%20effect%2C%20and%20quantum%20wires.">work</a> .</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Scientists use field theory to understand tiny-micro particles. One part of this theory, called Quantum Field Theory (QFT), combined ideas from fast-moving things and tiny things to explain many things, like strange anti-particles and the Higgs field.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Then, statistical mechanics and condensed matter physics connect us to heat, gases, and changes between different states which, they might seem different.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>But it gets interesting when we look at two equations that pop up in both fields. In QFT, we calculate averages using a path like this:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":1053,"width":507,"height":204,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/08/lagrida_latex_editor-18.png?w=221" alt="" class="wp-image-1053" style="width:507px;height:204px" width="507" height="204"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>And in condensed matter physics, we find averages like this:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":1055,"width":522,"height":245,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/08/lagrida_latex_editor-1-5.png?w=198" alt="" class="wp-image-1055" style="width:522px;height:245px" width="522" height="245"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>These equations look pretty similar, even though they're from different areas. This shows there's something connecting them.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>In the upcoming posts, we'll see how field theory affects things in condensed matter. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list {"ordered":true} -->
+<ol><!-- wp:list-item -->
+<li><a href="https://wordpress.com/post/everlastingquestforphysics.wordpress.com/1060">Approaches in the Study of Field Theory : Φ<sup>4</sup> theory-Part A</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://wordpress.com/post/everlastingquestforphysics.wordpress.com/1133">Approaches in the Study of Field Theory : Φ<sup>4</sup> theory-Part B</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://wordpress.com/post/everlastingquestforphysics.wordpress.com/1202">Application in Condensed Matter: The Ising Model- Part A</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://everlastingquestforphysics.wordpress.com/2023/08/16/field-theory-applications-in-condensed-matter-physics-iib/">Application in Condensed Matter: The Ising Model-PartB</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://everlastingquestforphysics.wordpress.com/2023/08/16/field-theory-applications-in-condensed-matter-physics-iii/">Study of Other Statistical Field Theories</a></li>
+<!-- /wp:list-item --></ol>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>So, get ready for some easy reading where we'll dive into this exciting topic. Stay tuned for more!</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":1057,"width":671,"height":168,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="https://everlastingquestforphysics.files.wordpress.com/2023/08/image-19.png?w=1024" alt="" class="wp-image-1057" style="width:671px;height:168px" width="671" height="168"/></figure>
+<!-- /wp:image -->
